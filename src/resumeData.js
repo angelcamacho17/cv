e522ee9b754c8fc24567ed22e644517cda349869
angelcamacho17 <angelcamacho1708@gmail.com>
2019-08-30 11:19:56 -0300

    web

--- src/resumeData.js
+++ src/resumeData.js
@@ -19,7 +19,7 @@ let resumeData = {
       ],
     "aboutme":"I’m currently developing and maintaining a powerful resource management application for wholesale companies in cache language (like a mini SAP), and also independently full-stack developing a webapp with spring boot, mysql and react. As a freelancer, I collaborate and strongly improve an insurance administrator webapp, which was developed in php. In college I’ve achieved to make the best copy of a game (Laps Fuse) and the professor was so thrilled with my work that he ask me to tutor and help him in the programming course after that. I’m 21 years old and I study software engineering (3rd year).",
     "address":"Cordoba, Argentina",
-    "website":"https://rbhatia46.github.io",
+    "website":"",
     "education":[
       {
         "UniversityName":"Universidad Siglo 21",

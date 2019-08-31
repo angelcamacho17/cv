8968765090c7f4f922f6b0a174d7691ba0333f15
Angel <angelcamacho1708@gmail.com>
2019-08-31 16:35:39 -0300

    Updates

--- precache-manifest.5519b46c00126c0ccc896f7a9689272e.js
+++ precache-manifest.5519b46c00126c0ccc896f7a9689272e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/resume/static/js/runtime~main.18bea22a.js"
   },
   {
-    "revision": "f1f8a74ac6a0a6f7ed9b",
-    "url": "/resume/static/js/main.f1f8a74a.chunk.js"
+    "revision": "c8321cffb3aba95a0ede",
+    "url": "/resume/static/js/main.c8321cff.chunk.js"
   },
   {
     "revision": "a7194b2d988a5c80f583",
     "url": "/resume/static/js/1.a7194b2d.chunk.js"
   },
   {
-    "revision": "f1f8a74ac6a0a6f7ed9b",
+    "revision": "c8321cffb3aba95a0ede",
     "url": "/resume/static/css/main.d97dd3db.chunk.css"
   },
   {
-    "revision": "ee82732bdbd0d2222f97088a9816ae3d",
+    "revision": "82d5eacfb63986313614e9b16ef24066",
     "url": "/resume/index.html"
   }
 ];
